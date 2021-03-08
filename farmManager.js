75788133d0ed00f77a092e5cc1dc53e6a8ac603f
jaitone <jaitoone@gmail.com>
2021-03-08 15:21:05 +0000

    Improved formatting

--- farmManager.js
+++ farmManager.js
@@ -1,36 +1,41 @@
-const Field = require("./field")
 const ChemicalProvider = require("./chemicalProvider")
-
-let cowfield = new Field("Cowfield", "300 metres", "400 metres", "Barley", "18th February")
-let top = new Field("Top", "200 metres", "600 metres", "Barley", "22nd December")
-let hillside = new Field("Hillside", "600 metres", "400 metres", "Winter Wheat", "5th February")
-let bigField = new Field("Big Field", "800 metres", "500 metres", "Sugar Beet", "9th January")
-let upperRiver = new Field("Upper River", "200 metres", "200 metres", "Barley", "18th January")
-let lowerRiver = new Field("Lower River", "500 metres", "100 metres", "Sugar Beet", "8th February")
-let southside = new Field("South Side", "400 metres", "400 metres", "Winter Wheat", "12th January")
 
 class FarmManager {
   constructor() {
-    this.fields = [cowfield, top, hillside, bigField, upperRiver, lowerRiver, southside]
+    this.fields = []
     this.currentField;
     this.currentChemical;
     this.chemicalProvider = new ChemicalProvider
     this.chemicalsNames = []
   }
 
+  addField = (field) => {
+    this.fields.push(field)
+  }
 
+  selectField = (fieldName) => {
 
-  checkField = (fieldName) => {
+    let logField;
     this.fields.forEach(field => {
       if (field.name === fieldName) {
         this.currentField = field
+        logField = field.name + "    ||    " + field.width + "    ||    " + field.length + "    ||    " + field.crop + "    ||    " + field.lastSprayed
       }
     })
-
+    console.log(logField)
     return this.currentField
   }
 
+  checkAllFields = () => {
+    console.log("Name        ||     Width     ||     Length     ||     Crop     ||     Last Sprayed")
+    console.log("")
+    this.fields.map(field => {
+      console.log(field.name + "    ||    " + field.width + "    ||    " + field.length + "    ||    " + field.crop + "    ||    " + field.lastSprayed)
+    })
+  }
+
   checkLastSprayed = () => {
+    console.log(this.currentField.lastSprayed)
     return this.currentField.lastSprayed
   }
 
@@ -38,19 +43,15 @@ class FarmManager {
     let hectares = this.currentField.getHectares()
     let pricePerLitre = this.currentChemical.getPricePerLitre()
     let totalPrice = hectares * pricePerLitre
-    return `£${totalPrice}`
+    console.log(`£${totalPrice}`)
 
   }
 
   checkChemicalsFromProvider = () => {
     let chemicals = this.chemicalProvider.getChemicals()
-    let names;
-    chemicals.map(chemical => {
-      names = chemical.getChemicalName()
-      console.log(names)
+    chemicals.forEach(chemical => {
+      console.log(chemical.name)
     })
-    // return
-    // console.log(this.chemicalProvider)
   }
 
 
@@ -63,6 +64,7 @@ class FarmManager {
         this.currentChemical = chemical
       }
     })
+    console.log("Chemical ordered " + this.currentChemical.name)
     return this.currentChemical
   }
 
@@ -70,20 +72,33 @@ class FarmManager {
     let date = this.currentField.getFormatedDate()
     console.log(date)
     let frequency = this.currentChemical.getApplicationsFrequency().replace(/\D/g, "")
-    
+    console.log(frequency, "Frequency")
     let weeks = parseInt(frequency) * 7
 
     let arrivalDate = date.setDate(date.getDate() + weeks)
+  
     // .replace(/\D/g, "")
+    console.log(new Intl.DateTimeFormat('en-US').format(arrivalDate));
     console.log(new Date(arrivalDate))
+    console.log(new Date(arrivalDate).getDate().toString())
     console.log(frequency.replace(/\D/g, ""))
+    console.log(new Date(arrivalDate).getMonth())
   }
 
 
-
-
-
-
+  formatReadableDate = (day) => {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
 }
 
 module.exports = FarmManager
--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const FarmManager = require("./farmManager")
-
-
-
-
-
-
-
-
-
-
-// let appleTreeFarm = new Farm([cowfield, top, hillside, bigField, upperRiver, lowerRiver, southside], [ryezapon, berbelikar, quadranis])
+const Field = require("./field")
 farmManager = new FarmManager
-console.log(farmManager.checkField("Cowfield"))
-console.log(farmManager.checkLastSprayed())
-console.log(farmManager.checkChemicalsFromProvider())
-console.log(farmManager.orderChemical("Ryezapon"))
-console.log(farmManager.checkChemicalArrival())
-// console.log(farmManager.calculateTotalPrice())
+
+let cowfield = new Field("Cowfield", "300 metres", "400 metres", "Barley", "18th February")
+let top = new Field("Top", "200 metres", "600 metres", "Barley", "22nd December")
+let hillside = new Field("Hillside", "600 metres", "400 metres", "Winter Wheat", "5th February")
+let bigField = new Field("Big Field", "800 metres", "500 metres", "Sugar Beet", "9th January")
+let upperRiver = new Field("Upper River", "200 metres", "200 metres", "Barley", "18th January")
+let lowerRiver = new Field("Lower River", "500 metres", "100 metres", "Sugar Beet", "8th February")
+let southside = new Field("South Side", "400 metres", "400 metres", "Winter Wheat", "12th January")
+
+
+
+
+farmManager.addField(cowfield)
+farmManager.addField(top)
+farmManager.addField(hillside)
+farmManager.addField(bigField)
+farmManager.addField(upperRiver)
+farmManager.addField(lowerRiver)
+farmManager.addField(southside)
+
+
+
+console.log("")
+farmManager.checkAllFields()
+
+console.log("")
+console.log("Selecting a new Field")
+farmManager.selectField("Cowfield")
+
+console.log("")
+console.log("Checking Last spread")
+farmManager.checkLastSprayed()
+
+console.log("")
+console.log("Checking chemicals from provider")
+farmManager.checkChemicalsFromProvider()
+
+console.log("")
+console.log("Order new chemical")
+farmManager.orderChemical("Ryezapon")
+
+console.log("")
+console.log("Arriving")
+farmManager.checkChemicalArrival()
+
+console.log("")
+console.log("Total Price")
+farmManager.calculateTotalPrice()
